b72d6c2383cdeae63641d62c024743f46d19dd0c
huongtt-175 <huongtt@mor.com.vn>
2020-12-26 15:48:01 +0700

    fix style css modal add chunha

--- src/components/ManageChuNha/Popups/PopupAddChuNha.vue
+++ src/components/ManageChuNha/Popups/PopupAddChuNha.vue
@@ -2,131 +2,229 @@
   <b-modal :id="idModal" no-close-on-backdrop :title="titleModal" size="xl">
     <div class="popup-add-chunha">
       <div class="justify-content-between">
-        <div class="form-input d-flex width-33">
-          <label for="fullName" class="width-50"><span class="text-color-red">*</span> Tên chủ nhà:</label>
-          <b-form-input placeholder="" id="fullName" ></b-form-input>
-        </div>
-
-        <div class="form-input d-flex width-33">
-          <label for="dob" class="width-50">Ngày sinh:</label>
-          <b-form-input placeholder="" id="dob" ></b-form-input>
-        </div>
-
-        <div class="form-input d-flex width-33">
-          <label for="address" class="width-50">Địa chỉ:</label>
-          <b-form-input placeholder="" id="address" ></b-form-input>
-        </div>
+        <b-row>
+          <b-col cols="4" class="d-flex">
+            <label for="fullName" class="align-items-center w-50"><span class="text-color-red">*</span> Tên chủ nhà:</label>
+            <b-form-input placeholder="" id="fullName" ></b-form-input>
+          </b-col>
+          <b-col cols="4" class="d-flex">
+            <label for="dob" class="align-items-center w-50">Ngày sinh:</label>
+            <b-form-input placeholder="" id="dob" ></b-form-input>
+          </b-col>
+          <b-col cols="4" class="d-flex">
+            <label for="address" class="align-items-center w-50">Địa chỉ:</label>
+            <b-form-input placeholder="" id="address" ></b-form-input>
+          </b-col>
+        </b-row>
       </div>
 
       <div>
-        <h1 class="header mt-5 mb-5">THÔNG TIN LIÊN LẠC</h1>
-        <div class="form-input d-flex width-60 mb-4">
-          <div class="d-flex width-60">
-            <label for="phoneNumber" class="width-60 align-items-center" ><span class="text-color-red">*</span> Số điện thoại:</label>
-            <b-form-input placeholder="" id="phoneNumber" ></b-form-input>
-          </div>
+        <h2 class="header mt-5 mb-5">THÔNG TIN LIÊN LẠC</h2>
+        <b-row>
+          <b-col class="d-flex" cols="4">
+            <label for="phoneNumber" class="align-items-center w-50"><span class="text-color-red">*</span> Số điện thoại:</label>
+            <b-form-input placeholder="" id="phoneNumber"></b-form-input>
+          </b-col>
 
-          <div class="ml-4 align-items-center">
+          <b-col class="d-flex" cols="4">
             <b-form-checkbox
               id="checkbox-phone"
               name="checkbox-phone"
+              class="w-50 align-items-center"
+              v-model="phoneNumberSub"
             >
               SDT dự phòng
             </b-form-checkbox>
-          </div>
-        </div>
+            <b-form-input placeholder="" id="phoneNumber-sub" v-show="phoneNumberSub"></b-form-input>
+          </b-col>
+        </b-row>
 
-        <div class="form-input d-flex width-60">
-          <div class="d-flex width-60">
-            <label for="email" class="width-60 align-items-center" >Email:</label>
+        <b-row class="mt-5">
+          <b-col class="d-flex" cols="4">
+            <label for="email" class="align-items-center w-50"><span class="text-color-red">*</span> Email:</label>
             <b-form-input placeholder="" id="email" ></b-form-input>
-          </div>
+          </b-col>
 
-          <div class="ml-4 align-items-center">
+          <b-col class="d-flex" cols="4">
             <b-form-checkbox
               id="checkbox-email"
               name="checkbox-email"
+              class="w-50 align-items-center"
+              v-model="emailSub"
             >
               Email dự phòng
             </b-form-checkbox>
-          </div>
-        </div>
+            <b-form-input placeholder="" id="email-sub" v-show="emailSub"></b-form-input>
+          </b-col>
+        </b-row>
       </div>
 
       <div>
-        <h1 class="header mt-5 mb-5">THÔNG TIN GIẤY TỜ TÙY THÂN</h1>
-        <div>
-          <b-form-checkbox
-            id="checkbox-cmnd"
-            name="checkbox-cmnd"
-            class="mb-2"
-          >
-            Chứng minh thư nhân dân
-          </b-form-checkbox>
+        <h2 class="header mt-5 mb-5">THÔNG TIN GIẤY TỜ TÙY THÂN</h2>
+        <b-row>
+          <b-col cols="2">
+            <b-form-checkbox
+              id="checkbox-cmnd"
+              name="checkbox-cmnd"
+              class="mb-2"
+              v-model="checkboxCMT"
+            >
+              Chứng minh thư nhân dân
+            </b-form-checkbox>
+          </b-col>
 
-          <b-form-checkbox
-            id="checkbox-cccd"
-            name="checkbox-cccd"
-            class="mb-2"
-          >
-            Căn cước công dân
-          </b-form-checkbox>
+          <b-col cols="3" class="d-flex">
+            <div v-show="checkboxCMT" class="d-flex">
+              <label for="cmnd" class="align-items-center w-50">Số CMND:</label>
+              <b-form-input placeholder="" id="cmnd"></b-form-input>
+            </div>
+          </b-col>
 
-          <b-form-checkbox
-            id="checkbox-hc"
-            name="checkbox-hc"
-            class="mb-2"
-          >
-            Hộ chiếu
-          </b-form-checkbox>
-        </div>
+          <b-col cols="3" class="d-flex">
+            <div  v-show="checkboxCMT" class="d-flex">
+              <label for="noicap-cmnd" class="align-items-center w-50">Nơi cấp:</label>
+              <b-form-input placeholder="" id="noicap-cmnd"></b-form-input>
+            </div>
+          </b-col>
+
+          <b-col cols="4" class="d-flex">
+            <div v-show="checkboxCMT" class="d-flex col">
+              <label for="ngaycap-cmnd" class="align-items-center w-50">Ngày cấp:</label>
+              <b-form-datepicker id="ngaycap-cmnd"></b-form-datepicker>
+            </div>
+          </b-col>
+        </b-row>
+
+        <b-row class="mt-5">
+          <b-col cols="2">
+            <b-form-checkbox
+              id="checkbox-cccd"
+              name="checkbox-cccd"
+              class="mb-2"
+              v-model="checkboxCCCD"
+            >
+              Căn cước công dân
+            </b-form-checkbox>
+          </b-col>
+
+          <b-col cols="3" class="d-flex">
+            <div v-show="checkboxCCCD" class="d-flex">
+              <label for="cccd" class="align-items-center w-50">Số CCCD:</label>
+              <b-form-input placeholder="" id="cccd"></b-form-input>
+            </div>
+          </b-col>
+
+          <b-col cols="3" class="d-flex">
+            <div v-show="checkboxCCCD" class="d-flex">
+              <label for="noicap-cccd" class="align-items-center w-50">Nơi cấp:</label>
+              <b-form-input placeholder="" id="noicap-cccd"></b-form-input>
+            </div>
+          </b-col>
+
+          <b-col cols="4" class="d-flex">
+            <div v-show="checkboxCCCD" class="d-flex col">
+              <label for="ngaycap-cccd" class="align-items-center w-50">Ngày cấp:</label>
+              <b-form-datepicker id="ngaycap-cccd"></b-form-datepicker>
+            </div>
+          </b-col>
+        </b-row>
+
+        <b-row class="mt-5">
+          <b-col cols="2">
+            <b-form-checkbox
+              id="checkbox-hc"
+              name="checkbox-hc"
+              class="mb-2"
+              v-model="checkboxHC"
+            >
+              Hộ chiếu
+            </b-form-checkbox>
+          </b-col>
+
+          <b-col cols="3" class="d-flex">
+            <div v-show="checkboxHC" class="d-flex">
+              <label for="hc" class="align-items-center w-50">Số hộ chiếu:</label>
+              <b-form-input placeholder="" id="hc"></b-form-input>
+            </div>
+          </b-col>
+
+          <b-col cols="3" class="d-flex">
+            <div v-show="checkboxHC" class="d-flex col p-0">
+              <label for="ngaycap-hc" class="align-items-center w-50">Ngày cấp:</label>
+              <b-form-datepicker id="ngaycap-hc"></b-form-datepicker>
+            </div>
+          </b-col>
+
+          <b-col cols="4" class="d-flex">
+            <div v-show="checkboxHC" class="d-flex col">
+              <label for="ngayhethan-hc" class="align-items-center w-50">Ngày hết hạn:</label>
+              <b-form-datepicker id="ngayhethan-hc"></b-form-datepicker>
+            </div>
+          </b-col>
+        </b-row>
       </div>
 
       <div>
-        <h1 class="header mt-5 mb-5">THÔNG TIN TÀI KHOẢN NGÂN HÀNG</h1>
-        <div>
-          <div class="d-flex justify-content-between mb-3">
-            <div class="d-flex">
-              <label for="accountBank" class="width-60 align-items-center" >Số Tk:</label>
-              <b-form-input placeholder="" id="accountBank" ></b-form-input>
-            </div>
+        <h2 class="header mt-5 mb-5">THÔNG TIN TÀI KHOẢN NGÂN HÀNG</h2>
+        <b-row>
+          <b-col cols="3" class="d-flex">
+            <label for="accountBank" class="align-items-center w-50">Số Tk:</label>
+            <b-form-input placeholder="" id="accountBank"></b-form-input>
+          </b-col>
+          <b-col cols="3" class="d-flex">
+            <label for="bank" class="align-items-center w-50">Ngân hàng:</label>
+            <b-form-input placeholder="" id="bank"></b-form-input>
+          </b-col>
+          <b-col cols="3" class="d-flex">
+            <label for="branch" class="align-items-center w-50">Chi nhánh:</label>
+            <b-form-input placeholder="" id="branch"></b-form-input>
+          </b-col>
+        </b-row>
 
-            <div class="d-flex">
-              <label for="accountName" class="width-60 align-items-center" >Tên Tk:</label>
-              <b-form-input placeholder="" id="accountName" ></b-form-input>
-            </div>
-            
-            <div class="d-flex">
-              <label for="bank" class="width-60 align-items-center" >Ngân hàng:</label>
-              <b-form-input placeholder="" id="bank" ></b-form-input>
-            </div>
-
-            <div class="d-flex">
-              <label for="branch" class="width-60 align-items-center" >Chi nhánh:</label>
-              <b-form-input placeholder="" id="branch" ></b-form-input>
-            </div>
-          </div>
-
-          <div>
+        <b-row class="mt-5">
+          <b-col>
             <b-form-checkbox
               id="checkbox-account-bank-2"
               name="checkbox-account-bank-2"
-              class="mb-2"
+              v-model="bankAccountSub"
             >
               Tài khoản ngân hàng dự phòng
             </b-form-checkbox>
-          </div>
+          </b-col>
+        </b-row>
 
-          <div class="d-flex">
-            <label for="branch" class="width-10 align-items-center" >Ghi chú:</label>
+        <b-row class="mt-5">
+          <b-col cols="3" class="d-flex">
+            <div v-show="bankAccountSub" class="d-flex">
+              <label for="accountBank-sub" class="align-items-center w-50">Số Tk:</label>
+              <b-form-input placeholder="" id="accountBank-sub"></b-form-input>
+            </div>
+          </b-col>
+          <b-col cols="3" class="d-flex">
+            <div v-show="bankAccountSub" class="d-flex">
+              <label for="bank-sub" class="align-items-center w-50">Ngân hàng:</label>
+              <b-form-input placeholder="" id="bank-sub"></b-form-input>
+            </div>
+          </b-col>
+          <b-col cols="3" class="d-flex">
+            <div v-show="bankAccountSub" class="d-flex">
+              <label for="branch-sub" class="align-items-center w-50">Chi nhánh:</label>
+              <b-form-input placeholder="" id="branch-sub"></b-form-input>
+            </div>
+          </b-col>
+        </b-row>
+
+        <b-row class="mt-5">
+          <b-col class="d-flex">
+            <label for="textarea-note" class="align-items-center w-25">Ghi chú:</label>
             <b-form-textarea
-              id="textarea"
+              id="textarea-note"
               placeholder="Nhập ghi chú"
               rows="3"
               max-rows="6"
             ></b-form-textarea>
-          </div>
-        </div>
+          </b-col>
+        </b-row>
       </div>
     </div>
     <template #modal-footer="">
@@ -150,6 +248,16 @@ export default {
       type: String,
     },
   },
+  data() {
+    return {
+      phoneNumberSub: false,
+      emailSub: false,
+      checkboxCMT: false,
+      checkboxCCCD: false,
+      checkboxHC: false,
+      bankAccountSub: false,
+    };
+  },
   methods: {
     cancel() {
       this.$bvModal.hide(this.idModal);
@@ -160,28 +268,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.width-33 {
-  width: 33%;
-}
-.width-50 {
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  width: 50%;
-  padding-right: 12px;
-}
 .header {
   width: 40%;
   border-bottom: 3px solid #6a02e8;
   h1 {
     font-weight: 900;
   }
-}
-.width-60 {
-  width: 60%;
-}
-.width-10 {
-  width: 10%;
 }
 </style>
 
